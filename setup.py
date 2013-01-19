8296fc4266b331e1ef56580f9fa89286369ee741
Andrew Hekman <ajhekman@gmail.com>
2013-01-19 16:20:46 -0500

    Added classifiers

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ setup(name='oopen',
       version=version,
       description="Object-Oriented File and Path manipulation",
       long_description=open('README.rst','r').read(-1),
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers="""Development Status :: 3 - Alpha
+Intended Audience :: Developers
+Intended Audience :: System Administrators
+License :: OSI Approved :: MIT License
+Topic :: Software Development :: Libraries :: Python Modules""".split('\n'), # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='file path oo',
       author='Andrew Hekman',
       author_email='ajhekman@gmail.com',
