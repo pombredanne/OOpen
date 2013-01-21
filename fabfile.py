d7f6720b7b642ee1f05ba1bad1253d1e788a6dd7
Andrew Hekman <ajhekman@gmail.com>
2013-01-20 23:14:31 -0500

    Added new release support to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,15 @@
 from fabric.api import task, prompt, local
 from fabric.colors import green, red, blue
 from functools import partial
+from textwrap import dedent
 import oopen
 import sys
 import os
-
+import re
+import subprocess
+import datetime
 local = partial(local, capture=True)
+EDITOR = 'subl'
 
 
 def ensure_in_sync():
@@ -18,7 +22,7 @@ def update_version():
     current_version = oopen.__version__
     major, minor, patch = [int(n) for n in current_version.split('.')]
 
-    v_selection = prompt("Do you want to update the:\n(1) Major version: {0}\n(2) Minor version: {1}\n(3) Patch version: {2}\n=>".format(green(major), green(minor), green(minor)), default=3, validate='[0-9]')
+    v_selection = prompt("Do you want to update the:\n(1) Major version: {0}\n(2) Minor version: {1}\n(3) Patch version: {2}\n=>".format(green(major), green(minor), green(patch)), default="3", validate=r'[0-9]')
     v_selection = int(v_selection)
 
     v_selection -= 1
@@ -36,11 +40,33 @@ def update_version():
     if accept != "yes":
         sys.exit(1)
 
+    changes = local('git log --oneline --no-color {}..HEAD'.format(current_version))
     local(r'''sed -i "~" /^__version/s/\'.*\'/\\\'{}\\\'/ oopen/__init__.py'''.format(version))
 
+    change_log = re.sub(r'^[0-9a-z]*?\ ', '- ', changes)
+    # subprocess.check_output(popenargs, kwargs)
+    subproc = subprocess.Popen(['subl', '-n', '-w', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    retdata = subproc.communicate(change_log)[0]
 
-    # tag_selection = prompt(green("Do you want to issue a git tag -s ? [y]: ")).lower()
-    # tag_selection = True if tag_selection == 'y' else False
+    # subproc.stdin.write(change_log)
+    # retdata = subproc.stdout.read()
+    hist = open('HISTORY.rst', 'r')
+    line = ''
+    header = ''
+    #Set the position of the file
+    while line != 'History\n':
+        line = hist.readline()
+        header += line
+    header += hist.readline()
+
+    entry_header = dedent('''\
+
+        {0} ({1})
+        ++++++++++++++++++\n\n'''.format(version, str(datetime.date.today())))
+
+    new_hist = header + entry_header + retdata + "\n" + hist.read()
+    hist.close()
+    hist = open('HISTORY.rst', 'w').write(new_hist)
 
 
 @task
@@ -78,7 +104,7 @@ def push():
 def new_release():
 
     ans = local('git status --porcelain')
-    if ans == '':
+    if ans != '':
         print 'Please commit your code and run again.'
         return
 
